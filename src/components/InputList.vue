046485b6a767b82d172a02a75f08689e8edd3d5b
m-daichi1219 <matu9525@gmail.com>
2020-07-01 23:34:44 +0900

    Add v-bind sync

---
+++ src/components/InputList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <label for="editer">編集</label>
+    <input id="editer" v-model="dispEditText" />
+    <label for="target">対象</label>
+    <input id="target" v-model="dispTargettext"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class InputList extends Vue {
+  @Prop() private edittext!: string;
+
+  @Prop() private targettext!: string;
+
+  get dispEditText() {
+    return this.edittext;
+  }
+
+  set dispEditText(val: string) {
+    this.$emit('update:edittext', val);
+  }
+
+  get dispTargettext() {
+    return this.targettext;
+  }
+
+  set dispTargettext(val: string) {
+    this.$emit('update:targettext', val);
+  }
+}
+</script>
---
+++ src/components/SandBox.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>SandBox</h1>
+    <InputList :edittext.sync="inputObject.edittext" :targettext.sync="inputObject.targettext"/>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  Component, Prop, Vue, Watch,
+} from 'vue-property-decorator';
+import InputList from '@/components/InputList.vue';
+
+@Component({
+  data() {
+    return {
+      inputObject: {
+        edittext: 'edittext',
+        targettext: '',
+      },
+    };
+  },
+  components: {
+    InputList,
+  },
+})
+export default class SandBox extends Vue {
+  @Watch('inputObject.edittext')
+  changeEvent(val: any) {
+    console.log(val);
+    if (val.length > 10) {
+      (this as any).inputObject = { edittext: val, targettext: '10 over length' };
+    }
+  }
+}
+</script>
