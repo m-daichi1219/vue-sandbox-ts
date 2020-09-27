7fba3d33fd798c6201b91a10aa993f39d532e76d
m-daichi1219 <matu9525@gmail.com>
2020-09-27 23:06:43 +0900

    Add vue speedometer example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="home">
+  <div id="home" class="home">
     <chart-example />
+    <vue-speedometer
+     :maxValue="3000"
+     :customSegmentStops="[0, 500, 1000, 1500, 2000, 2500, 3000]"
+     :segmentColors='["#a3be8c","#ebcb8b","#d08770","#bf616a","#bf616a","#bf616a"]'
+     :value="700"/>
   </div>
 </template>
 
 <script>
 import ChartExample from '@/components/ChartExample.vue';
+import VueSpeedometer from 'vue-speedometer';
 
 export default {
   name: 'Home',
   components: {
     ChartExample,
+    VueSpeedometer,
   },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   publicPath: '/vue-sandbox-ts/',
   outputDir: 'docs',
+  runtimeCompiler: true,
 };
